3a45864f76322c699102f4a0900cc52095411c2d
saimaheedhar1 <143284328+saimaheedhar1@users.noreply.github.com>
2023-09-29 14:24:56 -0500

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,27 +8,72 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    reformated_dates = [datetime.strptime(old_dt, "%Y-%m-%d").strftime('%d %b %Y') for old_dt in old_dates]
+    return reformated_dates
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
-
+    if not isinstance(start, str) or not isinstance(n, int):
+        
+        raise TypeError()
+    
+    output_list = []
+    frmat = datetime.strptime(start, '%Y-%m-%d')
+    for a in range(n):
+        output_list.append(frmat + timedelta(days=a))
+    return output_list
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    date_range_len = date_range(start_date, len(values))
+    zipped_list = list(zip(date_range_len, values))
+    return zipped_list
 
+
+def helper(infile):
+    
+    headSet = ("book_uid,isbn_13,patron_id,date_checkout,date_due,date_returned".
+              split(','))
+    
+    with open(infile, 'r') as f:
+        c = DictReader(f, fieldnames=headSet)
+        all_rows = [row for row in c]
+
+        all_rows.pop(0)
+    
+    return all_rows
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    
+    rqrd_format = '%m/%d/%Y'
+    rows = helper(infile)
+    dict_for_latefee = defaultdict(float)
+
+    for row in rows:
+       
+        patron = row['patron_id']
+        due_on = datetime.strptime(row['date_due'], rqrd_format)
+        returned_on = datetime.strptime(row['date_returned'], rqrd_format)
+
+        late_days = (returned_on - due_on).days
+
+        dict_for_latefee[patron]+= 0.25 * late_days if late_days > 0 else 0.0
+
+    header = [
+        {'patron_id': pn, 'late_fees': f'{fs:0.2f}'} for pn, fs in dict_for_latefee.items()
+    ]
+
+    with open(outfile, 'w') as wrt:
+        dcrr = DictWriter(wrt, ['patron_id', 'late_fees'])
+        dcrr.writeheader()
+        dcrr.writerows(header)
 
 
 # The following main selection block will only run when you choose
